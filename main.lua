8289c801df839476caf84008f68785d7fca03674
odeebee <bargerybill@gmail.com>
2025-09-16 12:45:36 +0000

    Final commit

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 
 brickSprites = {}
 index = 1
+bgindex = 1
 moveTowerLeft = false
 startTowerPos = 125
 endTowerPos = 125 + 10*50 + 10
@@ -13,7 +14,9 @@ platformLength = {}
 platformDetractLength = {}
 platformDetractStarted = {}
 
-platformMaxDecreaseLength = 5
+platformMaxDecreaseLength = 0
+
+standardPlatformsLength = 50
 
 function love.load()
     getBrickImages()
@@ -26,6 +29,7 @@ function love.update()
 end
 
 function love.draw()
+    drawBackground()
     drawTower()
     drawPlatforms()
 end
@@ -50,6 +54,16 @@ function moveTower(key)
     end
 end
 
+function drawBackground()
+    for y=0,119 do
+        for x=0,800 do
+            love.graphics.setColor(255,255,0)
+            love.graphics.draw(brickSprites[bgindex],x*10,y*5)
+            love.graphics.setColor(255,255,255)
+        end
+    end
+end
+
 function drawTower()
     for y=0,119 do
         for x=0,50 do
@@ -60,6 +74,7 @@ function drawTower()
 end
 
 function updateTower()
+    print(bgindex)
     if moveTowerLeft == true then
         if index == 9 then
             index = 1
@@ -69,6 +84,12 @@ function updateTower()
     end
 
     if moveTowerLeft == false then
+        if bgindex == 9 then
+            bgindex = 1
+        else 
+            bgindex = bgindex + 1
+        end
+
         if index == 1 then
             index = 9
         else
@@ -87,7 +108,7 @@ function addPlatforms(range)
     for i=0,range do
         table.insert(platformX, startTowerPos + i * 50)
         print(platformX[i])
-        table.insert(platformY, i * 50)
+        table.insert(platformY, i * 50 + 50)
         table.insert(platformLength, 50)
         table.insert(platformDetractLength,false)
         table.insert(platformDetractStarted,false)
@@ -106,6 +127,7 @@ function addPlatforms(range)
     for i=1,#platformMovingRight do
         if platformMovingRight[i] == false then
             platformX[i] = platformX[i] - (platformX[i] - endTowerPos) * 2
+            platformLength[i] = platformMaxDecreaseLength
         end
     end
 
@@ -122,29 +144,76 @@ function updatePlatformPositions()
 
         movePlatforms(i)
 
+        detractPlatformLength(i)
+
+        arePlatformsFullyDetracted(i)
+
+        canPlatformsBeFlipped(i)
+
+        --printPlatformLength(i)
+        --print(platformX[1])
+    end
+end
+
+function canPlatformsBeFlipped(i)
+    if platformX[i] > 299 and platformX[i] < 301 and platformDetractStarted[i] == true and platformMovingRight[i] == true then
+        print("START FLIPPIN")
+        platformLength[i] = platformMaxDecreaseLength
+        platformLength[i] = standardPlatformsLength
+        platformX[i] = platformX[i] + -standardPlatformsLength
+        platformDetractStarted[i] = false
+    end
+end
+
+function printPlatformLength(i)
+    for i=1,#platformLength do
+        print(platformLength[i])
+    end
+end
+
+function arePlatformsFullyDetracted(i)
+    if platformLength[i] == platformMaxDecreaseLength and platformMovingRight[i] == true then
+        print("FINISHED")
+        platformMovingRight[i] = false
+    end
+
+    if platformLength[i] == -standardPlatformsLength and platformMovingRight[i] == false then
+        if platformMovingRight[i] == false then
+            --platformDetractStarted[i] = false
+            platformMovingRight[i] = true
+        end
+    end
+end
+
+function detractPlatformLength(i)
+    if platformDetractLength[i] == true and platformLength[i] > platformMaxDecreaseLength then
+        platformLength[i] = platformLength[i] - 1
+    end
+
+    if platformDetractStarted[i] == true and platformLength[i] > -standardPlatformsLength then
+        platformLength[i] = platformLength[i] - 1
     end
 end
 
 function arePlatformsAtBoundaries(i)
     if platformX[i] >= 635 and platformX[i] < 636.5 then
-        platformMovingRight[i] = false
-        -- if platformDetractStarted[i] == false then
-        --     platformDetractStarted[i] = true
-        --     platformDetractLength[i] = true
-        -- end
-
-        -- if platformDetractStarted[i] == false then
-        --     platformMovingRight[i] = false
-        -- end
+        --platformMovingRight[i] = false
+        if platformDetractLength[i] == false then
+            platformDetractLength[i] = true
+        end
     end
 
     if platformX[i] >= 124 and platformX[i] < 125.5 then
-        platformMovingRight[i] = true     
-    --     if platformDetractStarted[i] == false then
-    --         platformDetractLength[i] = true
-    --         platformDetractLength[i] = true
-    --     end
+        if platformDetractStarted[i] == false and platformDetractLength[i] == true then
+            platformDetractLength[i] = false
+            platformDetractStarted[i] = true
+            print("Im at the left side")
+        end
     end
+
+    -- if platformX[i] >= 124 and platformX[i] < 125.5 then
+    --     platformMovingRight[i] = true     
+    -- end
 end
 
 function movePlatforms(i)
